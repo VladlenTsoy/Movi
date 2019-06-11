de5cd4541fed80a9db0e888f409d35c8ca335c5a
Vladlen <vladlengg@gmail.com>
2019-06-11 05:25:16 +0500

    update trends block (grid less)

--- src/components/home/trending/Trending.tsx
+++ src/components/home/trending/Trending.tsx
@@ -1,7 +1,43 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import './Trending.less';
-import TrendingMoviesBlock from "./blocks/movies/Movies";
-import TrendingTvShowsBlock from "./blocks/tv-shows/TvShows";
+import {Button} from "antd";
+import {useStore} from "../../../store/useStore";
+import QueueAnim from "rc-queue-anim";
+import PosterBlock from "../../../layouts/blocks/poster/Poster";
+
+const Contents: React.FC<{ url: string, tab: number }> = ({url, tab}) => {
+    const {state} = useStore();
+    const [movies, setMovies]: any = useState([]);
+
+    useEffect(() => {
+        const fetch = async () => {
+            let {data} = await state.api.guest.get(`${url}`);
+            setMovies(data.results.slice(0, 7));
+        };
+
+        fetch().then();
+    }, [url]);
+
+    return <QueueAnim type={['bottom', 'top']} className="contents">
+        {[
+            movies.map((movie: any, key: number) =>
+                <div className="trend" key={key}>
+                    <PosterBlock
+                        position="landscape"
+                        image={{
+                            poster: `https://image.tmdb.org/t/p/${key === 0 ? 'w500' : 'w300'}/${movie.backdrop_path}`,
+                            alt: tab ? movie.title : movie.name
+                        }}
+                        info={{
+                            title: tab ? movie.title : movie.name,
+                            subTitle: ''
+                        }}
+                    />
+                </div>
+            )]}
+    </QueueAnim>
+};
+
 
 const Trending: React.FC = () => {
     let [currentTabMovie, setCurrentTabMovie] = useState(0);
@@ -10,7 +46,7 @@ const Trending: React.FC = () => {
         setCurrentTabMovie(tab);
 
     return <div className="trending layout-block">
-        <div className="tabs">
+        <div className="title-block">
             <div className="navs">
                 <span className={!currentTabMovie ? 'active' : ''}
                       onClick={() => changeTabs(0)}>Популярные Сериалы</span>
@@ -26,9 +62,16 @@ const Trending: React.FC = () => {
                 <span>За 3 месяца</span>
             </div>
         </div>
-        {
-            currentTabMovie ? <TrendingMoviesBlock/> : <TrendingTvShowsBlock/>
-        }
+
+        <Contents key={currentTabMovie ? "tv" : "movie"}
+                  tab={currentTabMovie}
+                  url={currentTabMovie ?
+                      '/movie/popular?api_key=ac98cb53e0760e1f61d042006ba12afa&language=ru&page=1'
+                      : '/tv/popular?api_key=ac98cb53e0760e1f61d042006ba12afa&language=ru&page=1'}/>
+
+        <div className="action">
+            <Button className="btn-for-block" type="ghost" block={true}>Показать еще</Button>
+        </div>
     </div>;
 };
 
