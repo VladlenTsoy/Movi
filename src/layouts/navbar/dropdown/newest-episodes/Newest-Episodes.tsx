65ca28eebdd0e9e0b2a22147162215579edbecce
Vladlen <vladlengg@gmail.com>
2019-06-26 02:40:44 +0500

    delete episodes block

--- src/layouts/navbar/dropdown/newest-episodes/Newest-Episodes.tsx
+++ src/layouts/navbar/dropdown/newest-episodes/Newest-Episodes.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import './Newest-Episodes.less';
 import {Col, Row} from "antd";
 import QueueAnim from 'rc-queue-anim';
-import EpisodeBlock from "../../../blocks/episode/Episode";
-import episode from '../../../../assets/movies/55d0c27bbdd079.89871566-340x192-c.jpg';
 
 const NewestEpisodesBlock: React.FC<{ kids: boolean }> = ({kids}) => {
     return <Col lg={kids ? 4 : 12} key="3">
@@ -17,12 +15,10 @@ const NewestEpisodesBlock: React.FC<{ kids: boolean }> = ({kids}) => {
                         {[
                             <Col lg={24} className="top-column" key={1}>
                                 <div className="episode">
-                                    <EpisodeBlock data={{image: episode, alt: ''}}/>
                                 </div>
                             </Col>,
                             <Col lg={24} className="top-column" key={2}>
                                 <div className="episode">
-                                    <EpisodeBlock data={{image: episode, alt: ''}}/>
                                 </div>
                             </Col>,
                         ]}
@@ -33,32 +29,26 @@ const NewestEpisodesBlock: React.FC<{ kids: boolean }> = ({kids}) => {
                         {[
                             <Col lg={8} className="top-column" key={1}>
                                 <div className="episode">
-                                    <EpisodeBlock data={{image: episode, alt: ''}}/>
                                 </div>
                             </Col>,
                             <Col lg={8} className="top-column" key={2}>
                                 <div className="episode">
-                                    <EpisodeBlock data={{image: episode, alt: ''}}/>
                                 </div>
                             </Col>,
                             <Col lg={8} className="top-column" key={3}>
                                 <div className="episode">
-                                    <EpisodeBlock data={{image: episode, alt: ''}}/>
                                 </div>
                             </Col>,
                             <Col lg={8} key={4}>
                                 <div className="episode">
-                                    <EpisodeBlock data={{image: episode, alt: ''}}/>
                                 </div>
                             </Col>,
                             <Col lg={8} key={5}>
                                 <div className="episode">
-                                    <EpisodeBlock data={{image: episode, alt: ''}}/>
                                 </div>
                             </Col>,
                             <Col lg={8} key={6}>
                                 <div className="episode">
-                                    <EpisodeBlock data={{image: episode, alt: ''}}/>
                                 </div>
                             </Col>,
                         ]}
