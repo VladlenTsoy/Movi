48117a0767a28ad7988065487699f16545288774
Vladlen <vladlengg@gmail.com>
2019-06-26 02:40:04 +0500

    update movies page add url

--- src/components/movies/Movies.tsx
+++ src/components/movies/Movies.tsx
@@ -1,13 +1,64 @@
-import React from "react";
-import Filter from "./filter/Filter";
+import React, {useEffect, useState} from "react";
+import "./Movies.less";
 import Genres from "./filter/genres/Genres";
+import {Col, Row, Icon} from "antd";
+import MoviesPosterBlock from "../../layouts/blocks/movie-poster/MoviePoster";
+import {useStore} from "../../store/useStore";
 
 const Movies: React.FC = () => {
+    const {state} = useStore();
+    let [genre, setGenre] = useState(12);
+    let [url, setUrl] = useState(`/discover/movie?with_genres=${genre}&api_key=ac98cb53e0760e1f61d042006ba12afa&language=ru&primary_release_year=2019&page=`);
+    const [apiPage, setApiPage] = useState(1);
+    // Current movies
+    const [movies, setMovies]: any = useState([]);
+    // Sort
+    let sortGenres = (_genre: number) => {
+        setGenre(_genre);
+        setUrl(`/discover/movie?with_genres=${_genre}&api_key=ac98cb53e0760e1f61d042006ba12afa&language=ru&primary_release_year=2019&page=`);
+    };
+
+    useEffect(() => {
+        const fetch = async () => {
+            let {data} = await state.api.guest.get(`${url}${apiPage}`);
+
+            apiPage === 1 ? setMovies(data['results']) :
+                setMovies((m: any) => [...m, ...data['results']]);
+        };
+
+        fetch().catch();
+    }, [apiPage, state.api.guest]);
+
+    console.log(movies);
     return <div className="movies">
-        <Filter>
-            <Genres>
-            </Genres>
-        </Filter>
+        <div className="breadcrumbs">
+            <span>Главная</span>
+            <span><Icon type="right"/></span>
+            <span>Фильмы</span>
+        </div>
+        <Row>
+            <Col span={6} className="wrapper-filter-genres">
+                <Genres/>
+            </Col>
+            <Col span={18} className="wrapper-movies-block">
+                <div className="title-block">
+                    <div className="title">Фильмы</div>
+                </div>
+                <div className="wrapper-movies-filter-block">
+                    <div className="movies-filter">
+
+                    </div>
+                </div>
+                <div className="wrapper-posters-block">
+                    {movies.map((movie: any, key: number) => {
+                        if ((key + 1) % 3)
+                            return <MoviesPosterBlock movie={movie} key={key}/>;
+                        else
+                            return <div className="line" key={key}/>;
+                    })}
+                </div>
+            </Col>
+        </Row>
     </div>;
 };
 
